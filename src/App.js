417fa39f8e96b1ce721d89ba8e52740102843fdb
“your <kushniruk.yana@gmail.com>
2024-06-06 12:22:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
+
+
+import { useState } from "react";
+import { dataTwo } from "./data";
+import {data} from "./data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [planet, setPlanet] = useState(data);
+
+const removePlanet = (id) => {
+  let newPlanet = planet.filter(sun => sun.id !== id); 
+ setPlanet(newPlanet)
 }
+
+
+
+const [facts, setFacts] = useState(0);
+  const {id, nameTwo, fact, imageTwo} = dataTwo[facts];
+
+  const previousPerson = () => {
+    setFacts (( facts => {
+      facts --;
+     if (facts < 0) {
+       return data.length -1;
+     }
+     return facts;
+    }))
+ }
+
+ const nextPerson = () => {
+   setFacts ((facts => {
+    facts ++;
+     if (facts> data.length -1) {
+      facts = 0;
+     }
+     return facts;
+   }))
+ }
+
+
+
+
+
+  return (<div>
+    <div className='main'>
+      <h1>Solar system {planet.lenth}</h1>
+      </div>
+      
+      {planet.map ((thing => {
+        const {id, name, description, information, image } = thing;
+
+
+        return (
+          <div key={id}> 
+            
+            <div className="container">
+              <h2 className="planet">{name}</h2>
+            </div>
+
+        
+
+            <div className="container">
+              <img className="pic" src= {image} alt="planets" width="500px"/>
+            </div>
+
+
+            <div className="container">
+            <h3 className="planetOne">{description}</h3>
+            </div>
+
+            <div className="container">
+            <p className="planetTwo"> {information}</p>
+            </div>
+
+
+
+          
+            <div className='container'>
+<button className="btn"onClick={() => removePlanet(id)}> Remove Planet</button>
+</div>
+
+          </div>
+        )
+      }))}
+<div className='container'>
+<button className="btn"onClick={() => setPlanet ([])}> Delete All Planets</button>
+</div>
+
+
+     <div className="mainOne" key={id}> 
+       
+     <div className='box'>
+      <h2 className="fact"> Facts about Planets</h2>
+      </div>
+       
+      <div className='container'>  
+         <h2 className="factOne"> {nameTwo}</h2>
+      </div>
+    
+      <div className='par'>
+      <button  className="btnOne"onClick={previousPerson} >Previous</button>
+          <img className="picture" src={imageTwo}  alt="fact"/>
+      <button className="btnOne" onClick={nextPerson}> Next</button>
+       </div>
+
+      <div className='container bottom'>  
+         <p className="factTwo"> {fact} </p>
+      </div>
+      
+ 
+
+    </div>
+  
+
+ </div>
+  );
+  
+}
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,149 @@
+export const data = [
+    {
+        id: 1,
+        name: "Mercury",
+        description: "Mercury is very different from other planets in our Solar System. ",
+        information: " Mercury is the smallest planet in the Solar System in both mass and diameter. It’s 18 times less massive than the Earth, and its diameter barely reaches two-fifths of the Earth’s size. To give you a better understanding of Mercury’s size, let’s say that it’s only a third larger than the Moon.",
+        image: "https://starwalk.space/gallery/images/facts-about-mercury/1140x641.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Venus",
+        description: " As the brightest planet in the sky, Venus has been known to astronomers since ancient times. ",
+        information: " Venus has a mean radius of 6,052 km (3,760 miles). A trip around the equator of this planet would cover a distance of about 38,025 km (23,627 miles). Unlike many other Solar System planets with an equatorial bulge, Venus is a nearly perfect sphere, so its radius doesn’t differ at the poles and the equator. The equatorial bulge doesn’t form because Venus rotates very slowly around its axis. ",
+        image: "https://starwalk.space/gallery/images/amazing-planet-venus/1140x641.jpg"
+    },
+
+    {
+        id: 3,
+        name: "Earth",
+        description: "Earth is the third planet from the Sun and the only astronomical object known to harbor life.  ",
+        information: " Earth is rounded into an ellipsoid with a circumference of about 40,000 km. It is the densest planet in the Solar System. Of the four rocky planets, it is the largest and most massive. Earth is about eight light-minutes away from the Sun and orbits it, taking a year (about 365.25 days) to complete one revolution. ",
+        image: "https://starwalk.space/gallery/images/earth-day-2022/1140x641.jpg"
+    },
+
+    {
+        id: 4,
+        name: "Mars",
+        description: " The red planet Mars has been known to humans since ancient times. It has been the target of dozens of space missions and will hopefully soon become the first planet astronauts will land on.",
+        information: " Mars is the second smallest planet in the Solar System – only Mercury is smaller. Mars has a diameter of 6,792 km (4,220 miles); the planet’s circumference around the equator is 21,326 km (13,300 miles). Thus, if you’re moving at a speed of about 100 km per hour (62 miles per hour), it will take you about nine days to travel around the planet's equator.",
+        image: "https://starwalk.space/gallery/images/mars-the-ultimate-guide/1140x641.jpg"
+    },
+
+    {
+        id: 5,
+        name: "Jupiter",
+        description: " Jupiter is one of the brightest planets in our skies and the largest and most massive planet in the Solar System. It has faint rings, numerous moons, and an unstable surface.",
+        information: " Jupiter is considered the giant or the Jovian planet, together with Saturn, Uranus, and Neptune. When ancient astronomers named Jupiter after the Roman ruler of all gods, they had no idea about its enormous size surpassing other planets. Yet, they came up with a very fitting name.",
+        image: "https://starwalk.space/gallery/images/jupiter-guide/1140x641.jpg"
+    },
+
+    {
+        id: 6,
+        name: "Saturn",
+        description: `Adorned with beautiful bright rings, Saturn is sometimes referred to as "the Jewel of the Solar System".`,
+        information: " Along with Jupiter, Uranus, and Neptune, Saturn is considered a giant planet. Let's discover some curious details about the size of this amazing planet. With a radius of 58,232 kilometers (36,184 miles), Saturn is the second-largest planet in our Solar System — only its fellow gas giant Jupiter is larger. To take a trip around the planet's equator, you would need to travel a distance of 365,882 kilometers (227,349 miles)!",
+        image: "https://starwalk.space/gallery/images/saturn-planet-guide/1140x641.jpg"
+    },
+
+    {
+        id: 7,
+        name: "Uranus",
+        description: " Uranus is the weirdest place among the eight Solar System planets — with its peculiar chemical composition, extreme temperatures, and the most unusual axial tilt. ",
+        information: ` Uranus belongs to giant planets. Imagine this: if the Earth were a large apple, Uranus would be the size of a basketball. However, the ice giant doesn’t look large from the Earth — Uranus’ apparent size varies from 3.3" to 4.1", while Mercury, the smallest planet, can appear to us from 4.5" to 13".`,
+        image: "https://starwalk.space/gallery/images/uranus-facts/1140x641.jpg"
+    },
+
+    {
+        id: 8,
+        name: " Neptune",
+        description: " Neptune is known as the eighth and farthest planet from the Sun, which can't be seen with the naked eye from the Earth. This distant blue planet has faint rings, numerous moons, and no solid surface.  ",
+        information: " As the outermost planet from the Sun in our Solar System, Neptune has a very small apparent size and is challenging to observe from the Earth. Fortunately, the scientific data allow us to imagine the size of this remote planet.",
+        image: "https://starwalk.space/gallery/images/neptune-at-opposition-2021/1140x641.jpg"
+    },
+
+    {
+        id: 9,
+        name: "  Pluto",
+        description: " For 76 years since its discovery, Pluto was considered the ninth planet of the Solar System until, in 2006, it was demoted to dwarf planet status. ",
+        information: " As Pluto is located very far from the Earth, little was known for sure about this celestial body until NASA’s New Horizons flew by it in 2015. The space probe discovered that Pluto has a diameter of 2,370 kilometers (1,473 miles). Despite its comparatively modest size, Pluto is the largest known object in the Solar System beyond the orbit of Neptune.",
+        image: "https://starwalk.space/gallery/images/pluto-guide/1140x641.jpg"
+    }
+]
+
+
+export const dataTwo = [
+    {
+      id: 1,
+      nameTwo: " Mercury",
+      fact: " Mercury is the smallest planet in our solar system. It’s just a little bigger than Earth’s Moon.",
+      imageTwo: " https://science.nasa.gov/wp-content/uploads/2023/05/pia19422-mercury.jpg?w=1280&format=webp",
+
+    },
+
+    {
+        id: 2,
+        nameTwo: " Venus",
+        fact: "Even though Venus isn't the closest planet to the Sun, it is still the hottest. It has a thick atmosphere full of the greenhouse gas carbon dioxide and clouds made of sulfuric acid.",
+        imageTwo: " https://images-assets.nasa.gov/image/PIA00109/PIA00109~orig.jpg?w=1250&h=1000&fit=clip&crop=faces%2Cfocalpoint",
+  
+      },
+
+      {
+        id: 3,
+        nameTwo: " Earth",
+        fact: "Our home planet Earth is a rocky, terrestrial planet. It has a solid and active surface with mountains, valleys, canyons, plains and so much more. Earth is special because it is an ocean planet. Water covers 70% of Earth's surface.",
+        imageTwo: " https://parade.com/.image/t_share/MTkwNTgxNDY5Mzk2NjczNjYx/earth-facts-jpg.jpg",
+  
+      },
+
+      {
+        id: 4,
+        nameTwo: " Mars",
+        fact: "Mars is one of the most explored bodies in our solar system, and it's the only planet where we've sent rovers to roam the alien landscape.",
+        imageTwo: "https://science.nasa.gov/wp-content/uploads/2023/06/mars-curiosity_1280-jpg.webp?w=1280&format=webp",
+  
+      },
+
+      {
+        id: 5,
+        nameTwo: " Jupiter",
+        fact: "Jupiter's signature stripes and swirls are actually cold, windy clouds of ammonia and water, floating in an atmosphere of hydrogen and helium. ",
+        imageTwo: "https://cdn.businessinsider.de/wp-content/uploads/2019/03/planet-jupiter-clouds-juno-perijove-13-may-2018-nasa-jpl-caltech-swri-msss-kevin-m-gill-275440626071a53463938k.jpg",
+  
+      },
+
+      {
+        id: 6,
+        nameTwo: " Saturn",
+        fact: "Saturn isn’t the only planet to have rings, but it definitely has the most beautiful ones. The rings we see are made of groups of tiny ringlets that surround Saturn. They’re made of chunks of ice and rock. Like Jupiter, Saturn is mostly a ball of hydrogen and helium",
+        imageTwo: "https://images-assets.nasa.gov/image/PIA11141/PIA11141~large.jpg?w=1920&h=929&fit=clip&crop=faces%2Cfocalpoint",
+  
+      },
+
+      {
+        id: 7,
+        nameTwo: "Uranus",
+        fact: "Uranus is made of water, methane, and ammonia fluids above a small rocky center. Its atmosphere is made of hydrogen and helium like Jupiter and Saturn, but it also has methane. The methane makes Uranus blue.",
+        imageTwo: "https://ichef.bbci.co.uk/images/ic/480xn/p0257vk5.jpg.webp",
+  
+      },
+
+
+      {
+        id: 8,
+        nameTwo: "Neptune",
+        fact: "Neptune is dark, cold, and very windy. It's the last of the planets in our solar system. It's more than 30 times as far from the sun as Earth is. Neptune is very similar to Uranus. It's made of a thick fog of water, ammonia, and methane over an Earth-sized solid center.",
+        imageTwo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRldAU2bwCSAVteM_kuW9VOQGxzj4mmfyiWA&s",
+  
+      },
+
+      {
+        id: 9,
+        nameTwo: " Pluto",
+        fact: "First thing’s first… Is Pluto a planet? In 2006, Pluto was reclassified from a planet… to a dwarf planet. Pluto is now categorized as a dwarf planet along with other objects in the solar system ",
+        imageTwo: "https://science.nasa.gov/wp-content/uploads/2023/04/137119main2_hst_pluto1_516-jpg.webp?w=1280&format=webp",
+  
+      },
+]
